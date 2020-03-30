e4a0202bcd2f04d14ed795d60fdd465e0608dbf6
Dev Jana <devjana@Devs-MBP-2.domain>
2020-03-29 19:41:04 -0500

    client side for POST

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,43 @@
-console.log( 'js' );
+$( document ).ready( onReady );
+
+function addTask(){
+    console.log( 'in addTask' );
+    // get user input
+    //create an object to send
+    const objectToSend = {
+        task: $( '#taskIn' ).val()
+    }
+    console.log( 'sending:', objectToSend );
+    // send obj to server
+    $.ajax({
+        type: 'POST',
+        url: '/items',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+        // update DOM if successful
+        getTasks();
+    }).catch( function( err ){
+        alert( 'unable to add task. see console for more' );
+        // show error if not
+        console.log( err );
+    }) // end AJAX
+} // end addTask
+
+function completeTask(){
+    console.log( 'in completeTask' );
+} // end completeTask
+
+function deleteTask(){
+    console.log( 'in deleteTask' );
+} // end deleteTask
+
+function getTasks(){
+    console.log( 'in getTasks' );
+} // end getTasks
+
+function onReady(){
+    $( '#addTaskButton' ).on( 'click', addTask );
+    // get tasks from server
+    getTasks();
+} // end onReady
