1e23a6722968073e4d2cd8957a20b241117e890c
Dev Jana <devjana@Devs-MBP-2.domain>
2020-03-29 20:31:53 -0500

    fixed small then error in DELETE route

--- server/routes/items.js
+++ server/routes/items.js
@@ -42,8 +42,8 @@ router.delete( '/:id', ( req, res )=>{
     let queryString = `DELETE FROM "to-do" WHERE id= ($1)`;
     pool.query( queryString, [ req.params.id ] ).then( ( result )=>{
         res.sendStatus( 200 );
-    }).then( ( err )=>{
-        res.sendStatus( 200 );
+    }).catch( ( err )=>{
+        res.sendStatus( 500 );
     }) //end query
 }) //end DELETE
 // exports
